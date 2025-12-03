10ebdbd7cc57d330a179c73061de144d22a85349
Damien Spielmann <damien.spielmann@hotmail.com>
2025-12-03 20:17:55 +0100

    Use firebase-admin send-push endpoint

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,4 @@
-// api/ping.js
+// api/ping.js 03.12.2025
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
